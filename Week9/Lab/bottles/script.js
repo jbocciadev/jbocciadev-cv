988d054a5ba5d7741134e2b7800b57f915226092
jbocciadev <jboccia@gmail.com>
2025-11-22 11:21:40 +0000

    Implement styling in 99 bottles of beer.

--- Week9/Lab/bottles/script.js
+++ Week9/Lab/bottles/script.js
@@ -5,16 +5,18 @@ let button = document.getElementById("songButton");
 let btnContainer = document.getElementById("buttonContainer");
 let songContainer = document.getElementById("songContainer");
 
-button.addEventListener("click", function (){
-    let song = ``;
+button.addEventListener("click", function () {
+    let song = `<h1>99 Bottles of Beer!</h1>`;
     btnContainer.style.display = "none";
     songContainer.style.display = "flex";
-    for (i=99; i>0; i--){
-        if (i > 1){
-            song += `<br> ${i} bottles of beer on the wall, ${i} bottles of beer <br> Take one down and pass it around, ${i-1} bottles of beer on the wall`
+    for (i = 99; i > 0; i--) {
+        if (i > 1) {
+            song += `<p> ${i} bottles of beer on the wall, ${i} bottles of beer... <br> Take one down and pass it around, ${
+                i - 1
+            } bottles of beer on the wall!</p>`;
         } else {
-            song += `<br> ${i} bottle of beer on the wall, ${i} bottle of beer <br> Take one down and pass it around, no more bottles of beer on the wall
-            <br> No more bottles of beer on the wall, no more bottles of beer <br> Go to the store and buy some more, 99 bottles of beer on the wall`
+            song += `<p> ${i} bottle of beer on the wall, ${i} bottle of beer... <br> Take one down and pass it around, no more bottles of beer on the wall!
+            <br> No more bottles of beer on the wall, no more bottles of beer... <br> Go to the store and buy some more, 99 bottles of beer on the wall</p>`;
         }
         songContainer.innerHTML = song;
     }
