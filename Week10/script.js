96a16bf6be4769d6785b0504be3280a529a5e14c
jbocciadev <jboccia@gmail.com>
2025-11-27 12:41:14 +0000

    change addToList for a loop and implement removeFromList function.

--- Week10/script.js
+++ Week10/script.js
@@ -1,18 +1,33 @@
 let shoppingList = [];
+let listDisplay = document.getElementById("list");
 document.getElementById("add-btn").addEventListener("click", () => {
-    console.log("clicked!");
     let newItem = prompt("What would you like to add to the list?");
     shoppingList.push(newItem);
-    addToList(newItem);
+    displayList();
 });
 
-function addToList(newItem) {
-    let listDisplay = document.getElementById("list");
-    let listItm = document.createElement("li");
-    let itmBtn = document.createElement("button");
-    listItm.innerHTML = `${newItem}`;
-    // itmBtn.dataset.index = index;
-    itmBtn.innerHTML = "-";
-    listItm.appendChild(itmBtn);
-    listDisplay.appendChild(listItm);
+function displayList() {
+    listDisplay.innerHTML = "";
+    for ([index, item] of shoppingList.entries()) {
+        let listItm = document.createElement("li");
+        let itmBtn = document.createElement("button");
+        listItm.innerHTML = `${item}`;
+        itmBtn.dataset.index = index;
+        itmBtn.setAttribute("onclick", `removeFromList(event)`);
+        itmBtn.innerHTML = "-";
+        listItm.appendChild(itmBtn);
+        listDisplay.appendChild(listItm);
+    }
+}
+
+function removeFromList(event) {
+    let targetItem = event.target.parentElement;
+    let targetIndex = event.target.dataset.index;
+    listDisplay.removeChild(targetItem);
+    if (shoppingList.length > 0) {
+        shoppingList.splice(targetIndex, 1);
+    } else {
+        shoppingList = [];
+    }
+    displayList();
 }
