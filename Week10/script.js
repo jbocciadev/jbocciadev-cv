f721e8c2b5720472d6efcd650d8be2f5a0322c9b
jbocciadev <jboccia@gmail.com>
2025-11-27 13:32:53 +0000

    Implement final changes and styling.

--- Week10/script.js
+++ Week10/script.js
@@ -2,19 +2,27 @@ let shoppingList = [];
 let listDisplay = document.getElementById("list");
 document.getElementById("add-btn").addEventListener("click", () => {
     let newItem = prompt("What would you like to add to the list?");
-    shoppingList.push(newItem);
-    displayList();
+    if (
+        newItem != "" &&
+        shoppingList.indexOf(newItem) == -1 &&
+        newItem != null
+    ) {
+        shoppingList.push(newItem);
+        displayList();
+    }
 });
 
 function displayList() {
     listDisplay.innerHTML = "";
     for ([index, item] of shoppingList.entries()) {
         let listItm = document.createElement("li");
+        let itmTxt = document.createElement("p");
         let itmBtn = document.createElement("button");
-        listItm.innerHTML = `${item}`;
+        itmTxt.innerText = `${item}`;
         itmBtn.dataset.index = index;
         itmBtn.setAttribute("onclick", `removeFromList(event)`);
         itmBtn.innerHTML = "-";
+        listItm.appendChild(itmTxt);
         listItm.appendChild(itmBtn);
         listDisplay.appendChild(listItm);
     }
